d1d97dedc1e424974acd396ef423f6a7166507ae
Mathijs de Lepper <mathijsdelepper@gmail.com>
2015-04-30 15:36:57 +0200

    verbeterd algoritme

--- Opdracht2/PrimeSieve.py
+++ Opdracht2/PrimeSieve.py
@@ -7,23 +7,25 @@ T1 = time.perf_counter()
 
 file = sys.argv[2]
 fh = open(file, "w")
+i=0
 
 def SoE(N):
-    zeef = list(range(2, N))
+    zeef = list(range(2,N))
     
-    for x in range(2, N+1):
-        for y in range(x, ceil(N/x)):
-            getal = x*y
-            if getal in zeef:
-                zeef.remove(getal)
+    for x in zeef:
+        if x != 0:
+            for y in range(x, ceil(N/x)):
+                zeef[x*y - 2] = 0
     return zeef
 
 N = int(sys.argv[1])
 mijnZeef = SoE(N)
 
 for k in mijnZeef:
-    fh.write(str(k) +'\n')
+    if k != 0:
+        i += 1
+        fh.write(str(k) +'\n')
 
 T2 = time.perf_counter()
 
-print ('Found ' + str(len(mijnZeef)) +' Prime numbers smaller than ' +str(N)+ ' in '+str(T2-T1)+' seconds')
+print ('Found ' + str(i) +' Prime numbers smaller than ' +str(N)+ ' in '+str(T2-T1)+' seconds')
