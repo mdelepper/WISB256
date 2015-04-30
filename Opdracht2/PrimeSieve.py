164a63e51d20b609bdd5d056e14251cbd9ddb560
Mathijs de Lepper <mathijsdelepper@gmail.com>
2015-04-30 14:34:56 +0200

    nu werkend in command line

--- Opdracht2/PrimeSieve.py
+++ Opdracht2/PrimeSieve.py
@@ -1,23 +1,29 @@
 import time
 import sys
+import pickle
 from math import ceil
 
 T1 = time.perf_counter()
 
+file = sys.argv[2]
+fh = open(file, "w")
+
 def SoE(N):
-    zeef = list(range(1, N))
+    zeef = list(range(2, N))
     
     for x in range(2, N+1):
-        for y in range(2, ceil(N/x)):
+        for y in range(x, ceil(N/x)):
             getal = x*y
             if getal in zeef:
                 zeef.remove(getal)
     return zeef
 
+N = int(sys.argv[1])
+mijnZeef = SoE(N)
 
-# N = sys.argv[1]
-# mijnZeef = SoE(N)
-
-# python PrimeSieve.py 15000 primes.data
+for k in mijnZeef:
+    fh.write(str(k) +'\n')
 
 T2 = time.perf_counter()
+
+print ('Found ' + str(len(mijnZeef)) +' Prime numbers smaller than ' +str(N)+ ' in '+str(T2-T1)+' seconds')
