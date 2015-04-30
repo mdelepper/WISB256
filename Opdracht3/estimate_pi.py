4b47535fd4e1a933054fb945fd7e14bbc301987b
Mathijs de Lepper <mathijsdelepper@gmail.com>
2015-04-30 19:21:27 +0200

    bijna af!!

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -2,13 +2,14 @@ import random
 import math
 import sys
 
-N = int(sys.argv[1]) #aantal experimenten
-l = float(sys.argv[2]) #lengte van naald
+try:
+    N = int(sys.argv[1])                            #aantal experimenten
+    l = float(sys.argv[2])                          #lengte van naald
+    h = 0                                           #aantal hits
+except:
+    print ('Use: python3 estimate_pi.py N L')
 
-assert l <= 1
-
-
-h = 0 #aantal hits
+assert(l < 1), "L should be smaller than 1"
 
 def drop_needle(L):
     x_1 = random.random()
@@ -23,9 +24,9 @@ def drop_needle(L):
         return False
 
 for x in range(N):
-    if drop_needle(x) == True:
+    if drop_needle(x) == True :
         h += 1
 
 pi = (2*l*N)/h
 
-print('Pi is ongeveer ' + str(pi))
+print(str(h) + ' hits in ' + str(N) + ' tries \n' +'Pi = ' + str(pi))
