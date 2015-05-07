9f77df7419659d450492e866f4ff198a4f2d854f
Mathijs de Lepper <mathijsdelepper@gmail.com>
2015-05-07 15:24:44 +0200

    kleine aanpassing voor snelheid

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -8,12 +8,12 @@ try:
     h = 0                                           #aantal hits
 except:
     print ('Use: python3 estimate_pi.py N L')
+    exit()
 
 assert(l <= 1), "L should be smaller than 1"
 
 def drop_needle(L):
     x_1 = random.random()
-    y_2 = random.random()
     hoek = random.vonmisesvariate(0,0)
 
     x_2 = x_1 + l*math.cos(hoek)
