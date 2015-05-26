0a20ca8d809e93237ad05197c4095d53e6a10b40
Mathijs de Lepper <mathijsdelepper@gmail.com>
2015-05-26 11:49:00 +0200

    af op bonus na

---
+++ Opdracht5/Vector.py
@@ -0,0 +1,48 @@
+import math
+
+class Vector:
+
+
+    def __init__(self, n, a = 0.0):
+        if type(a) == list:
+            self.v = a
+        else:
+            self.v = [a]*n
+
+    def __str__(self):
+        vector = ''
+        for item in self.v:
+            vector = vector + "{0:.6f}".format(item)+'\n'
+            
+        return str(vector)
+
+    def lincomb(self, other, alpha, beta):
+        w = Vector(len(self.v))
+    
+        w = self.scalar(alpha) + other.scalar(beta)
+        return w
+
+    def __add__(self, other):
+        w = Vector(len(self.v))
+        for i in range(len(self.v)):
+            w.v[i] = self.v[i] + other.v[i]
+        return w
+
+    def scalar(self, alpha):
+        w = Vector(len(self.v))
+        for i in range(len(w.v)):
+            w.v[i]= alpha*self.v[i]
+
+        return w
+
+    def inner(self, other):
+        res = 0.0
+        for i in range(len(self.v)):
+            res += self.v[i]*other.v[i]
+        return "{0:.6f}".format(res)
+
+    def norm(self):
+        res = self.inner(self)
+        return "{0:.6f}".format(math.sqrt(float(res)))
+
+    
