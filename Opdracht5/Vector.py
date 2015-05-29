2c0bee420e895b4e0658069470552c2892044d11
Mathijs de Lepper <mathijsdelepper@gmail.com>
2015-05-29 18:00:27 +0200

    kleine aanpassing

--- Opdracht5/Vector.py
+++ Opdracht5/Vector.py
@@ -28,6 +28,12 @@ class Vector:
             w.v[i] = self.v[i] + other.v[i]
         return w
 
+    def __sub__(self, other):
+        w = Vector(len(self.v))
+        for i in range(len(self.v)):
+            w.v[i] = self.v[i] + other.v[i]
+        return w
+
     def scalar(self, alpha):
         w = Vector(len(self.v))
         for i in range(len(w.v)):
@@ -44,5 +50,12 @@ class Vector:
     def norm(self):
         res = self.inner(self)
         return "{0:.6f}".format(math.sqrt(float(res)))
-
-    
+   
+def GrammSchmidt(V):
+    lijst = V
+    for i in range(len(lijst)):
+        lijst[i] = lijst[i].scalar(1/( float(lijst[i].norm()) ))
+        for j in range(i+1, len(lijst)):
+            ratio = float( lijst[i].inner(lijst[j]) ) / float(lijst[j].norm())
+            lijst[j] = lijst[j] - lijst[j].scalar(ratio)
+        return lijst
